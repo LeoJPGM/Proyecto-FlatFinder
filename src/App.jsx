1959d7f93b1dd5818c0b1296614484aacd2957a1
LeoJPGM <leojp4781@gmail.com>
2024-08-07 19:15:52 -0500

    1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Filters } from "./components/Filters";
+
 function App() {
-  return <h1 className="text-blue-800">Texto de prueba</h1>;
+  return (
+    <>
+      <Filters />
+    </>
+  );
 }
 
 export default App;
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,3 +1,42 @@
 export const Filters = () => {
-  return <div>Filters</div>;
+  return (
+    <>
+      <div className="filter h-screen border-5 border-gray-300 border-r pl-5">
+        <h1 className="text-3xl">FILTROS</h1>
+        <div className="py-4">
+          <h3>CIUDAD</h3>
+          <input
+            className="border-2 px-1 py-1 w-60"
+            placeholder="Seleccione la ciudad"
+          />
+          <h3>PRECIO</h3>
+          <input
+            className="border-2 px-1 py-1 w-20 text-center"
+            placeholder="MIN"
+          />
+          <input
+            className="border-2 px-1 py-1 w-20 text-center"
+            placeholder="MAX"
+          />
+          <h3>TAMAÑO DEL AREA</h3>
+          <input
+            className="border-2 px-1 py-1 w-20 text-center"
+            placeholder="MIN"
+          />
+          <input
+            className="border-2 px-1 py-1 w-20 text0center"
+            placeholder="MAX"
+          />
+        </div>
+        <h1 className="text-3xl">CATEGORIAS</h1>
+        <div className="py-4">
+          <h3>ORDENAR POR CIUDAD</h3>
+          <input
+            className="border-2 px-1 py-1 w-60"
+            placeholder="Seleccione la ciudad"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
